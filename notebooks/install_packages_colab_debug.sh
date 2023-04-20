1ac645021cb789ede12b18de09258c83b37909c6
achri19 <alexandra.l.christensen@jpl.nasa.gov>
2023-04-20 15:13:04 -0700

    Update install_packages_colab_debug.sh

--- notebooks/install_packages_colab_debug.sh
+++ notebooks/install_packages_colab_debug.sh
@@ -4,8 +4,8 @@
 mkdir -p /content/drive/MyDrive/installations
 cd /content/drive/MyDrive/installations
 echo "(1) Install pip packages to /content/drive/MyDrive/installations"
-echo "nose mpi4py triangle dill Pmw pymetis anuga mpi4py gdal netcdf4 geemap cmocean geopandas fiona pygeos rasterio rasterstats scikit-fmm rtree pyTMD Orinoco"
-pip install --target=/content/drive/MyDrive/installations nose mpi4py pyproj triangle dill Pmw pymetis anuga mpi4py gdal netcdf4 geemap cmocean geopandas fiona pygeos rasterio rasterstats scikit-fmm rtree backports.zoneinfo pyTMD git+https://github.com/simard-landscape-lab/orinoco.git #> /dev/null 2>&1
+echo "nose mpi4py triangle dill Pmw pymetis mpi4py gdal netcdf4 pyproj geemap cmocean geopandas fiona pygeos rasterio rasterstats scikit-fmm rtree pyTMD Orinoco"
+pip install --target=/content/drive/MyDrive/installations nose mpi4py pyproj triangle dill Pmw pymetis mpi4py gdal netcdf4 geemap cmocean geopandas fiona pygeos rasterio rasterstats scikit-fmm rtree backports.zoneinfo pyTMD git+https://github.com/simard-landscape-lab/orinoco.git #> /dev/null 2>&1
 
 # echo "(2) Install gdal"
 # apt-get -q -y install --target=/content/drive/MyDrive/installations python-gdal gdal-bin  > /dev/null 2>&1
@@ -15,13 +15,13 @@ pip install --target=/content/drive/MyDrive/installations nose mpi4py pyproj tri
 
 echo "(3) Download anuga_core github repository"
 echo "https://github.com/GeoscienceAustralia/anuga_core"
-git clone https://github.com/GeoscienceAustralia/anuga_core.git  > /dev/null 2>&1
+git clone https://github.com/GeoscienceAustralia/anuga_core.git  #> /dev/null 2>&1
 
 echo "(4) Install anuga"
 
 cd anuga_core
-python setup.py --quiet build  > /dev/null 2>&1
-python setup.py --quiet install  > /dev/null 2>&1
+python setup.py build #--quiet build  > /dev/null 2>&1
+python setup.py install #--quiet install  > /dev/null 2>&1
 
 cd ../
 
