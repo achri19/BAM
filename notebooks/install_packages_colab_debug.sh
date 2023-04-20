c560ba91aa4a83c5bbd38e29059267bbc7e661a4
achri19 <alexandra.l.christensen@jpl.nasa.gov>
2023-04-20 15:15:17 -0700

    Update install_packages_colab_debug.sh

--- notebooks/install_packages_colab_debug.sh
+++ notebooks/install_packages_colab_debug.sh
@@ -5,13 +5,13 @@ mkdir -p /content/drive/MyDrive/installations
 cd /content/drive/MyDrive/installations
 echo "(1) Install pip packages to /content/drive/MyDrive/installations"
 echo "nose mpi4py triangle dill Pmw pymetis mpi4py gdal netcdf4 pyproj geemap cmocean geopandas fiona pygeos rasterio rasterstats scikit-fmm rtree pyTMD Orinoco"
-pip install --target=/content/drive/MyDrive/installations nose mpi4py pyproj triangle dill Pmw pymetis mpi4py gdal netcdf4 geemap cmocean geopandas fiona pygeos rasterio rasterstats scikit-fmm rtree backports.zoneinfo pyTMD git+https://github.com/simard-landscape-lab/orinoco.git #> /dev/null 2>&1
+pip install --target=/content/drive/MyDrive/installations nose mpi4py pyproj triangle dill Pmw pymetis mpi4py netcdf4 geemap cmocean geopandas fiona pygeos rasterio rasterstats scikit-fmm rtree backports.zoneinfo pyTMD git+https://github.com/simard-landscape-lab/orinoco.git #> /dev/null 2>&1
 
-# echo "(2) Install gdal"
-# apt-get -q -y install --target=/content/drive/MyDrive/installations python-gdal gdal-bin  > /dev/null 2>&1
+echo "(2) Install gdal"
+apt-get -y install --target=/content/drive/MyDrive/installations python-gdal gdal-bin  > /dev/null 2>&1
 
-# echo "(3) Install netcdf4"
-# apt-get -q -y install --target=/content/drive/MyDrive/installations python-netcdf4  > /dev/null 2>&1
+echo "(3) Install netcdf4"
+apt-get -y install --target=/content/drive/MyDrive/installations python-netcdf4  > /dev/null 2>&1
 
 echo "(3) Download anuga_core github repository"
 echo "https://github.com/GeoscienceAustralia/anuga_core"
