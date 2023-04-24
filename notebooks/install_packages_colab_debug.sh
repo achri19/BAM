9f748c596588f5192908c4c7bbb2a86e1e86add1
achri19 <alexandra.l.christensen@jpl.nasa.gov>
2023-04-24 09:22:48 -0700

    Update install_packages_colab_debug.sh

--- notebooks/install_packages_colab_debug.sh
+++ notebooks/install_packages_colab_debug.sh
@@ -5,14 +5,14 @@ mkdir -p /content/drive/MyDrive/installations
 cd /content/drive/MyDrive/installations
 echo "(1) Install pip packages to /content/drive/MyDrive/installations"
 echo "nose mpi4py triangle dill Pmw pymetis anuga mpi4py gdal netcdf4 geemap cmocean geopandas fiona pygeos rasterio rasterstats scikit-fmm rtree pyTMD Orinoco"
-pip install --target=/content/drive/MyDrive/installations nose mpi4py triangle dill Pmw pymetis mpi4py netcdf4 geemap cmocean geopandas fiona pygeos rasterio rasterstats scikit-fmm rtree backports.zoneinfo pyTMD git+https://github.com/simard-landscape-lab/orinoco.git
+pip install -q --target=/content/drive/MyDrive/installations nose mpi4py triangle dill Pmw pymetis mpi4py netcdf4 geemap cmocean geopandas fiona pygeos rasterio rasterstats scikit-fmm rtree backports.zoneinfo pyTMD git+https://github.com/simard-landscape-lab/orinoco.git
 #> /dev/null 2>&1
 
 #conda install -c conda-forge libspatialindex=1.9.3
 
 echo "(2) Install gdal"
 apt-get -q -y install --target=/content/drive/MyDrive/installations python-gdal gdal-bin  > /dev/null 2>&1
-apt-get -y install --target=/content/drive/MyDrive/installations libgdal-dev
+apt-get -q -y install --target=/content/drive/MyDrive/installations libgdal-dev
 
 # echo "(3) Install netcdf4"
 # apt-get -q -y install --target=/content/drive/MyDrive/installations python-netcdf4  > /dev/null 2>&1
