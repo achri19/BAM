bed420a89602534d7ed3230b9822698cb6050d52
achri19 <alexandra.l.christensen@jpl.nasa.gov>
2023-04-24 10:05:42 -0700

    Update install_packages_colab_debug.sh

--- notebooks/install_packages_colab_debug.sh
+++ notebooks/install_packages_colab_debug.sh
@@ -6,21 +6,24 @@ echo "(1) Install pip packages to /content/drive/MyDrive/installations"
 echo "nose mpi4py triangle dill Pmw pymetis mpi4py pyproj geemap cmocean geopandas fiona pygeos rasterio rasterstats scikit-fmm rtree pyTMD Orinoco"
 pip install --target=/content/drive/MyDrive/installations nose rtree shapely numpy mpi4py pyproj triangle dill Pmw pymetis mpi4py geemap cmocean geopandas fiona pygeos rasterio rasterstats scikit-fmm scipy backports.zoneinfo pyTMD git+https://github.com/simard-landscape-lab/orinoco.git > /dev/null 2>&1
 
+
 echo "(2) Install gdal"
 apt-get -q -y install --target=/content/drive/MyDrive/installations python-gdal gdal-bin  > /dev/null 2>&1
 
 echo "(3) Install netcdf4"
 apt-get -q -y install --target=/content/drive/MyDrive/installations python-netcdf4  > /dev/null 2>&1
 
-echo "(3) Download anuga_core github repository"
-echo "https://github.com/GeoscienceAustralia/anuga_core"
+echo "(4) Download anuga_core github repository"
+echo "htps://github.com/GeoscienceAustralia/anuga_core"
 git clone https://github.com/GeoscienceAustralia/anuga_core.git  #> /dev/null 2>&1
 
 echo "(4) Install OpenBlas"
 apt-get -y install --target=/content/drive/MyDrive/installations libopenblas-dev > /dev/null 2>&1
 
-echo "(5) Install anuga"
+echo "(5) Install Orinoco"
+pip install --target=/content/drive/MyDrive/installations git+https://github.com/simard-landscape-lab/orinoco.git > /dev/null 2>&1
 
+echo "(6) Install anuga"
 cd anuga_core
 python setup.py --quiet build  > /dev/null 2>&1
 python setup.py --quiet install  > /dev/null 2>&1
