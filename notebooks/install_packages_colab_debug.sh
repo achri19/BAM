3effa2a60dcb8b2a64acb7b1ff9b18d727502d74
achri19 <alexandra.l.christensen@jpl.nasa.gov>
2023-04-25 16:35:57 -0700

    Update install_packages_colab_debug.sh

--- notebooks/install_packages_colab_debug.sh
+++ notebooks/install_packages_colab_debug.sh
@@ -10,11 +10,11 @@ cd /content/drive/MyDrive/installations
 
 echo "(1) Install pip packages to /content/drive/MyDrive/installations"
 echo "nose mpi4py triangle dill Pmw pymetis mpi4py pyproj gdal geemap cmocean geopandas fiona pygeos rasterio rasterstats scikit-fmm rtree pyTMD Orinoco"
-pip install -q --target=/content/drive/MyDrive/installations nose rtree shapely numpy==1.23 pandas~=1.5.3 ipython~=7.34.0 ipykernel~=5.5.6 decorator<5.0,>=4.0.2 mpi4py triangle dill Pmw pymetis geemap cmocean geopandas netCDF4 pyproj fiona pygeos rasterio rasterstats scikit-fmm scipy backports.zoneinfo > /dev/null 2>&1
+pip install --target=/content/drive/MyDrive/installations nose rtree shapely numpy==1.23 pandas~=1.5.3 ipython~=7.34.0 ipykernel~=5.5.6 decorator<5.0,>=4.0.2 mpi4py triangle dill Pmw pymetis geemap cmocean geopandas netCDF4 pyproj fiona pygeos rasterio rasterstats scikit-fmm scipy backports.zoneinfo #> /dev/null 2>&1
 
 
 echo "(3) Install gdal"
-apt-get -y install gdal-bin python3-gdal #> /dev/null 2>&1
+apt-get -q -y install gdal-bin python3-gdal > /dev/null 2>&1
 # # apt-get -q -y install --target=/content/drive/MyDrive/installations libgdal-dev > /dev/null 2>&1
 # # apt-get -q -y install --target=/content/drive/MyDrive/installations python3-rtree > /dev/null 2>&1
 
