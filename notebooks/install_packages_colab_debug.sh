492605b5d1556b8d7912ed546bc614e346236890
achri19 <alexandra.l.christensen@jpl.nasa.gov>
2023-04-25 09:45:51 -0700

    Update install_packages_colab_debug.sh

--- notebooks/install_packages_colab_debug.sh
+++ notebooks/install_packages_colab_debug.sh
@@ -16,8 +16,8 @@ pip install -q --target=/content/drive/MyDrive/installations nose rtree shapely 
 
 
 echo "(3) Install gdal"
-apt-get -q -y install python-gdal gdal-bin python3-gdal > /dev/null 2>&1
-apt-get -q -y install libgdal-dev > /dev/null 2>&1
+apt-get -q -y install --target=/content/drive/MyDrive/installations python-gdal gdal-bin python3-gdal > /dev/null 2>&1
+apt-get -q -y install --target=/content/drive/MyDrive/installations libgdal-dev > /dev/null 2>&1
 
 echo "(4) Install netcdf4"
 apt-get -q -y install --target=/content/drive/MyDrive/installations python-netcdf4  > /dev/null 2>&1
